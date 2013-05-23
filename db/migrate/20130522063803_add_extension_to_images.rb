b7e86fb55898ab10ba6518353baacd6aa5ebbca0
Jason Larke <jasonlarke@live.com.au>
2013-05-23 20:36:41 +0800

    Updated migrations

--- db/migrate/20130522063803_add_extension_to_images.rb
+++ db/migrate/20130522063803_add_extension_to_images.rb
@@ -1,5 +1,5 @@
 class AddExtensionToImages < ActiveRecord::Migration
   def change
-    add_column :images, :extension, :string
+    add_column :images, :extension, :string, :limit => 10
   end
 end
